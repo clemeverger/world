e39c6ee9652de24e343f60942fc84d02b2591f1e
Clément Verger <clemeverger@gmail.com>
2023-02-03 17:26:57 +0100

    JPO

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Point, useTexture } from '@react-three/drei'
+import { useTexture } from '@react-three/drei'
 import React, { useRef, useMemo } from 'react'
 import vertexShader from './shaders/vertexShader.glsl'
 import fragmentShader from './shaders/fragmentShader.glsl'
@@ -8,48 +8,23 @@ import atmosphereFragment from './shaders/atmosphereFragment.glsl'
 
 import { AdditiveBlending } from 'three'
 import { BackSide } from 'three'
-import { useFrame, extend } from '@react-three/fiber'
+import { useFrame } from '@react-three/fiber'
 import { BufferAttribute } from 'three'
-import { DoubleSide } from 'three'
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
-import myFont from './shaders/montserrat.json'
-import gsap from 'gsap'
-
-extend({ TextGeometry })
-const font = new FontLoader().parse(myFont)
 
 const App = () => {
   const earthTexture = useTexture('earthmap10k.jpg')
 
   const groupRef = useRef()
   const earthRef = useRef()
-  /*   useFrame(({ clock, camera }) => {
-      
-      gsap.to(camera.position, {
-        x: 0, y: 0, z: 25, duration: 10, onUpdate: function () {
-          camera.lookAt(earthRef.current.position);
-        } 
-      })
-      groupRef.current.rotation.y += 0.0025;
-    }); */
+  useFrame(({ camera }) => {
+    groupRef.current.rotation.y += 0.0025
+    camera.position.z = 15 * Math.sin(Math.abs(groupRef.current.rotation.y))
+  })
 
   const points = useMemo(() => {
-    const p = new Array(1000).fill(0).map((v) => (0.5 - Math.random()) * 25)
+    const p = new Array(1000).fill(0).map((v) => (0.5 - Math.random()) * 15)
     return new BufferAttribute(new Float32Array(p), 3)
   }, [])
-
-  let earthLimit = [
-    { name: 'Le changement climatique', position: 0 },
-    { name: 'L’érosion de la biodiversité', position: 0.5 },
-    { name: 'Le changement d’utilisation des sols', position: 1 },
-    { name: 'L’introduction d’entités nouvelles dans la biosphère', position: 1.5 },
-    { name: 'La perturbation du cycle du phosphore et de l’azote', position: 2 },
-    { name: 'L’acidification des océans', position: 2.5 },
-    { name: 'L’augmentation des aérosols dans l’atmosphère', position: 3 },
-    { name: 'L’appauvrissement de  l’ozone stratosphérique', position: 3.5 },
-    { name: 'Le cycle d’eau douce bleue et verte', position: 4 },
-  ]
 
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 import App from './App'
 import './index.css'
 
@@ -9,18 +9,18 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
       <Canvas
-        camera={{ position: [25, 25, 50], fov: 15 }}
+        camera={{ position: [5, 5, 5], fov: 15 }}
         style={{
           backgroundColor: 'black',
           width: '100vw',
-          height: '100vh'
+          height: '100vh',
         }}
       >
         <ambientLight intensity={1} />
         <Suspense fallback={null}>
           <App></App>
         </Suspense>
-        <OrbitControls />
+        <OrbitControls enableZoom={false} />
       </Canvas>
     </>
   </React.StrictMode>
