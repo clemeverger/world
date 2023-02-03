c912950751a9245112419ea3e4b31c11ef8277ba
Clément Verger <clemeverger@gmail.com>
2023-02-03 18:19:35 +0100

    save

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
 import App from './App'
 import './index.css'
+import { OrbitControls } from '@react-three/drei'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -20,7 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Suspense fallback={null}>
           <App></App>
         </Suspense>
-        <OrbitControls enableZoom={false} />
+        <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} />
       </Canvas>
     </>
   </React.StrictMode>
